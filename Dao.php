e8a37c693aa54c74d2d879cac7206bcbe72b549f
PabloLomeli <51377489+PabloLomeli@users.noreply.github.com>
2020-03-28 15:32:49 -0600

    Update Dao.php

--- Dao.php
+++ Dao.php
@@ -6,11 +6,6 @@ class Dao {
   private $dbname = 'heroku_b2c5f55c93b1823';
   private $username = 'b2dde2f0af7810';
   private $password = 'aa1e7902';
-  private $logger;
-
-  public function __construct() {
-    $this->logger = new KLogger("log.txt", KLogger::WARN);
-  }
 
   public function getConnection() {
     try {
